eea7a71c8a884b6f0841a25d5cf692b2c8648835
ktkk <dezuttereluka@gmail.com>
2021-09-17 22:45:29 +0200

    Changed XYZ to RGB

--- src/color.c
+++ src/color.c
@@ -2,5 +2,5 @@
 
 void write_color(FILE* stream, color3* color) {
 	/* Write out the translated [0, 255] value of each color component */
-	fprintf(stream, "%d %d %d\n", CONVERT_COMPONENT(color->x), CONVERT_COMPONENT(color->y), CONVERT_COMPONENT(color->z));
+	fprintf(stream, "%d %d %d\n", CONVERT_COMPONENT(color->r), CONVERT_COMPONENT(color->g), CONVERT_COMPONENT(color->b));
 }
